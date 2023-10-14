9d989e886c947686207b09948d39b9e041f7aebb
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-10-14 11:21:25 +0000

    Deploying to gh-pages from @ MarynaLapa/goit-js-hw-08@8742c5f93e1d1e399a8686cd91c4bafd589b7978 🚀

---
+++ index.14f2e6d1.js
@@ -0,0 +1,2 @@
+console.log("hello"),console.log("hello");
+//# sourceMappingURL=index.14f2e6d1.js.map
---
+++ index.25c0148b.js
@@ -0,0 +1,2 @@
+console.log("hello"),console.log("hello");
+//# sourceMappingURL=index.25c0148b.js.map
