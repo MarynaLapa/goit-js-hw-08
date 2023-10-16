847dcc7aba6ec8ce300fd4f09ee794d3e55f87e0
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-10-16 18:23:28 +0300

    task-2 finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,53 +3,34 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-console.dir(player)
+// console.dir(player)
 
 const localStorageKey = "videoplayer-current-time";
 
-localStorage.setItem(localStorageKey, 10);
-
 player.on('timeupdate', throttle((data) => {
     const currentTime = data.seconds;
-    console.log(currentTime);
+    // console.log(currentTime);
     localStorage.setItem(localStorageKey, JSON.stringify(currentTime))
+    
+    
 }, 1000))
+function currentTime(key) {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? '0' : JSON.parse(serializedState);
+}
 
-// function timeUpDate(data) {
-//     const currentTime = data.seconds;
-//     console.log(currentTime);
-//     localStorage.setItem(localStorageKey, JSON.stringify(currentTime))
-// }
- 
+console.log(currentTime(localStorageKey))
 
-function onPlay(e) {
-        
-        // console.log(player.target);
+    player.setCurrentTime(currentTime(localStorageKey)).then((seconds) => {
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
     }
-    
-// console.log(player.on('timeupdate', throttle(data => {
-//   localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, data.seconds.toString());
-// }, 1000)));
-
-
-    // player.getVideoTitle().then(function(title) {
-    //     console.log('title:', title);
-    // });
-
-// player.setCurrentTime(35).then(function(seconds) {
-    
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-
-// console.log(player);
+});
