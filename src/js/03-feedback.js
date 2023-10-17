19e82208ae24f9a0e2f608ec0952a97c55180c34
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-10-17 18:55:04 +0300

    task-3 with a corrected error

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,13 +3,11 @@ import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 
 const localStorageKey = "feedback-form-state";
-let key_form = {};
-
-lodingForm();
+let key_form = { email: '', message: '' };
 
 form.addEventListener('input', throttle(handlerInput, 500));
 form.addEventListener("submit", handlerSubmit);
-
+lodingForm();
 
 function handlerInput(evt) {
     key_form[evt.target.name] = evt.target.value;
@@ -18,22 +16,23 @@ function handlerInput(evt) {
 
 function lodingForm() {
 
-    const saveForm = localStorage.getItem(localStorageKey);
-    
-        if (saveForm === null) {
-            form.elements.email.value = '';
-            form.elements.message.value = '';
-        } else {
-            const { email, message } = JSON.parse(saveForm);
-            form.elements.email.value = email;
-            form.elements.message.value = message;
-        }
-  
+const saveForm = localStorage.getItem(localStorageKey);
+
+    if (saveForm === null) {
+        form.elements.email.value = '';
+        form.elements.message.value = '';
+    } else {
+        const { email, message } = JSON.parse(saveForm);
+        form.elements.email.value = email;
+        form.elements.message.value = message;
+        // console.log({ email, message } );
+    }
 }
 
 function handlerSubmit(evt) {
     evt.preventDefault();
-    console.log(key_form);
+    const sumbitForm = localStorage.getItem(localStorageKey);
+    console.log(JSON.parse(sumbitForm));
     localStorage.removeItem(localStorageKey);
     form.reset();
 }
